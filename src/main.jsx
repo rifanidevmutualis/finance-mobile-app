bb28d22612af08030d82b2eab893e38e9f402505
Rifani <rifanidevmutualis@example.com>
2026-06-26 09:15:24 +0800

    fix: explicitly register service worker for PWA

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Import virtual module from vite-plugin-pwa to explicitly register Service Worker
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
